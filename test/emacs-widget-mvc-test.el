29373d6c399b598f4006d18600d17b6ce6cae155
AdrieanKhisbe <adriean.khisbe@live.fr>
2015-12-12 08:49:50 +0100

    Scaffold ert test

---
+++ test/emacs-widget-mvc-test.el
@@ -0,0 +1,10 @@
+;;; Test for `emacs-widget-mvc'
+
+;;; Commentary:
+;; These are the tests for `emacs-widget-mvc'
+
+;;; Code:
+
+(ert-deftest emacs-widget-mvc-should-not-pass ()
+  (should-not nil))
+
---
+++ test/test-helper.el
@@ -0,0 +1,41 @@
+;;; test-helper --- Test helper for emacs-widget-mvc
+
+;;; Commentary:
+;; test helper inspired from https://github.com/tonini/overseer.el/blob/master/test/test-helper.el
+
+;;; Code:
+
+(require 'f)
+
+(defvar cpt-path
+  (f-parent (f-this-file)))
+
+(defvar emacs-widget-mvc-test-path
+  (f-dirname (f-this-file)))
+
+(defvar emacs-widget-mvc-root-path
+  (f-parent emacs-widget-mvc-test-path))
+
+(defvar emacs-widget-mvc-sandbox-path
+  (f-expand "sandbox" emacs-widget-mvc-test-path))
+
+(when (f-exists? emacs-widget-mvc-sandbox-path)
+  (error "Something is already in %s. Check and destroy it yourself" emacs-widget-mvc-sandbox-path))
+
+(defmacro within-sandbox (&rest body)
+  "Evaluate BODY in an empty sandbox directory."
+  `(let ((default-directory emacs-widget-mvc-sandbox-path))
+     (when (f-exists? emacs-widget-mvc-sandbox-path)
+       (f-delete default-directory :force))
+     (f-mkdir emacs-widget-mvc-sandbox-path)
+     ,@body
+     (f-delete default-directory :force)))
+
+(require 'ert)
+(require 'el-mock)
+(eval-when-compile
+    (require 'cl))
+(require 'emacs-widget-mvc)
+
+(provide 'test-helper)
+;;; test-helper.el ends here
